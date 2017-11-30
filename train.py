2df9345856b1e4bd5587901be2d7899ccd45ce78
Quan Vuong <quan.vuong@nyu.edu>
2017-11-30 19:40:51 +0800

    print act_dist in run_episode if process rank i 0 and do not print in train policy net function

--- train.py
+++ train.py
@@ -83,11 +83,11 @@ def train_policy_net(policy_net, shared_policy_net, shared_policy_optim, episode
 
     returns = FloatTensorVar([step.G for step in episode])
 
-    if process_i == 0:
-        print(log_act_probs)
-        print(baselines)
-        print(returns)
-        sys.stdout.flush()
+    # if process_i == 0:
+    #     print(log_act_probs)
+    #     print(baselines)
+    #     print(returns)
+    #     sys.stdout.flush()
 
     # Call backward pass and update param
     shared_policy_optim.zero_grad()
