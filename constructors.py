7e9cf7e86a4b49b9a6c1a328d177021add6a68ee
Quan Vuong <quan.vuong@nyu.edu>
2017-11-30 18:28:31 +0800

    remove layers attribute from constructed network

--- constructors.py
+++ constructors.py
@@ -12,7 +12,6 @@ def build_value_net(args):
     :param layers: a list of size 3
         with index 0, 1, 2 specifying the input size, hidden layer size and output size respectively.
     :return: a FFN with 2 hidden layers and RELU non-linearity.
-        The returned object has attribute layers, which is the same as the input layers to this function.
     """
 
     class ValueNet(nn.Module):
@@ -24,7 +23,6 @@ def build_value_net(args):
             self.output_linear = nn.Linear(layers[1], layers[2])
             self.hid_linears = nn.ModuleList([nn.Linear(layers[1], layers[1])
                                               for _ in range(args.val_net_num_hid_layer)])
-            self.layers = layers
 
         def forward(self, state):
 
@@ -44,7 +42,6 @@ def build_policy_net(args):
     :param layers: a list of size 3
         with index 0, 1, 2 specifying the input size, hidden layer size and output size respectively.
     :return: a FFN with 1 hidden layer and RELU non-linearity followed by softmax.
-        The returned object has attribute layers, which is the same as the input layers to this function.
     """
 
     class PolicyNet(torch.nn.Module):
@@ -56,7 +53,6 @@ def build_policy_net(args):
             self.relu = torch.nn.ReLU()
             self.linear2 = torch.nn.Linear(layers[1], layers[2])
             self.sf = torch.nn.Softmax()
-            self.layers = layers
 
         def forward(self, state):
             before_sf = self.linear2(self.relu(self.linear1(state)))
